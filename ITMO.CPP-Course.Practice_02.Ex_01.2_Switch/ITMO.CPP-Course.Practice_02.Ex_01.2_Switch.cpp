02eefbfd472b91ca3213648a39cbae3a85700cbc
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-06-20 17:03:34 +0300

    добавлено Практика 2, упр. 2.2 - Switch

---
+++ ITMO.CPP-Course.Practice_02.Ex_01.2_Switch/ITMO.CPP-Course.Practice_02.Ex_01.2_Switch.cpp
@@ -0,0 +1,37 @@
+// ITMO.CPP-Course.Practice_02.Ex_01.2_Switch.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+	// руссификация консоли
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	std::cout << "Задание 2. Использование оператора switch при реализации выбора\n";
+	
+	char op;
+	std::cout << "Сделай свой выбор, собери авто своей мечты: ";
+	std::cin >> op;
+
+	switch (op)
+	{
+	case 'S':
+		std::cout << "Радио играть должно\n";
+		std::cout << "Колеса круглые\n";
+		std::cout << "Мощный двигатель\n";
+		break;
+	case 'V':
+		std::cout << "Кондиционер хочу\n";
+		std::cout << "Радио играть должно\n";
+		std::cout << "Колеса круглые\n";
+		std::cout << "Мощный двигатель\n";
+		break;
+	default:
+		std::cout << "Колеса круглые\n";
+		std::cout << "Мощный двигатель\n";
+	}
+
+}
